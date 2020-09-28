47b4083386e70554753f604b972c5bd60c584e9c
Yvo Keuter <ypmkeuter@gmail.com>
2020-09-28 23:16:14 +0200

    new scene with food

---
+++ Assets/Scripts/Food.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Food : MonoBehaviour
+{
+  [SerializeField] Material blue;
+  [SerializeField] Material red;
+  Renderer rend;
+
+  [SerializeField] float chargeTime = 5;
+  float timeLeft;
+  bool isActive = true;
+
+  // Start is called before the first frame update
+  void Start()
+  {
+    rend = GetComponent<Renderer>();
+  }
+
+    // Update is called once per frame
+  void Update()
+  {
+    if (!isActive)
+    {
+      if (timeLeft < 0)
+      {
+        isActive = true;
+        rend.material = blue;
+      }
+      timeLeft -= Time.deltaTime;
+    }
+  }
+
+  private void OnTriggerEnter(Collider other)
+  {
+    if (isActive)
+    {
+      isActive = false;
+      rend.material = red;
+      timeLeft = chargeTime;
+    }
+  }
+}
---
+++ Assets/Scripts/Landscape.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Xml;
+using UnityEngine;
+
+public class Landscape : MonoBehaviour
+{
+  [SerializeField] GameObject foodPrefab;
+  [SerializeField] int width = 100;
+  float height = 1.5f;
+  // Start is called before the first frame update
+  void Awake()
+  {
+    for (int i = 0; i < width; i++)
+    {
+      float x = (float)i - width / 2.0f + .5f;
+      for (int j = 0; j < width; j++)
+      {
+        float z = (float)j - width / 2.0f + .5f;
+        Instantiate(foodPrefab, new  Vector3(x, height, z), foodPrefab.transform.rotation);
+      }
+    }
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+        
+  }
+}
