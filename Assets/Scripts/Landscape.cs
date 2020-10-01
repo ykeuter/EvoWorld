0a5980c4dc7ec5aad81398bbe4295b7d526a286c
Yvo Keuter <ypmkeuter@gmail.com>
2020-10-01 16:50:15 +0200

    prevent falling through ground

--- Assets/Scripts/Landscape.cs
+++ Assets/Scripts/Landscape.cs
@@ -7,13 +7,13 @@ public class Landscape : MonoBehaviour
 {
   [SerializeField] GameObject foodPrefab;
   [SerializeField] int width = 100;
-  float height = 1.5f;
+  float height = 3.5f;
   // Start is called before the first frame update
   void Awake()
   {
-    for (int i = 0; i < width; i++)
+    for (int i = 0; i <= width; i++)
     {
-      float x = (float)i - width / 2.0f + .5f;
+      float x = (float)i - width / 2.0f;
       for (int j = 0; j < width; j++)
       {
         float z = (float)j - width / 2.0f + .5f;
