ce1e6050b83088e3738894e0594e05706a271371
Yvo Keuter <ypmkeuter@gmail.com>
2021-08-21 15:17:05 +0200

    work on search light script

--- Assets/Scripts/SearchLightAgent.cs
+++ Assets/Scripts/SearchLightAgent.cs
@@ -10,16 +10,36 @@ public class SearchLightAgent : Agent
 {
     [SerializeField] float speed = 2.0f;
     [SerializeField] float angularSpeed = 400.0f;
+    [SerializeField] GameObject target;
+    float size = 5;
+    float height;
+    bool idle = true;
+
+    private void Awake()
+    {
+        height = transform.localPosition.y;
+    }
+
+    public void ResetPlayer()
+    {
+        target.transform.localPosition = new Vector3(Random.Range(-size, size), 0, Random.Range(-size, size));
+        transform.localPosition = Vector3.up * height;
+        transform.localEulerAngles = Vector3.up * Random.Range(0, 360);
+        idle = false;
+    }
 
     public override void OnActionReceived(ActionBuffers vectorAction)
-    {
-        transform.Rotate(0, angularSpeed * vectorAction.ContinuousActions[0] * Time.fixedDeltaTime, 0);
-        transform.position += transform.forward * Time.fixedDeltaTime * speed * vectorAction.ContinuousActions[1];
+    {   if (!idle) {
+            transform.Rotate(0, angularSpeed * vectorAction.ContinuousActions[0] * Time.fixedDeltaTime, 0);
+            transform.position += transform.forward * Time.fixedDeltaTime * speed * vectorAction.ContinuousActions[1];
+        }
+        SetReward(Vector3.Distance(Vector3.ProjectOnPlane(transform.position, Vector3.up), target.transform.position));
     }
 
  
     private void OnCollisionEnter(Collision collision)
     {
+        idle = true;
         EndEpisode();
     }
 
