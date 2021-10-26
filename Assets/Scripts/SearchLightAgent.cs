6cc3e5ae9e1a3cc69d89325f0499e282ff44bfe0
Yvo Keuter <ypmkeuter@gmail.com>
2021-10-26 20:48:36 +0200

    scrolling set up

--- Assets/Scripts/SearchLightAgent.cs
+++ Assets/Scripts/SearchLightAgent.cs
@@ -9,14 +9,21 @@ using Unity.MLAgents.Actuators;
 public class SearchLightAgent : Agent
 {
     readonly float speed = 1.0f;
-    readonly float rotSpeed = 180.0f;
-    [SerializeField] GameObject target;
-    [SerializeField] GameObject [] obstacles;
-    Vector3 startPos;
-    readonly float bound = 2.0f;
-    readonly float margin = 1.25f;
+    //readonly float rotSpeed = 90.0f;
+    //[SerializeField] GameObject good;
+    //[SerializeField] GameObject bad;
+    [SerializeField] GameObject[] cases;
+    GameObject activeObject;
+    Vector3 startPosPlayer;
+    Vector3 startPosObject;
+    //readonly float bound = 2.0f;
+    //readonly float margin = 1.25f;
     readonly float reward = 1f;
-    readonly float punish = -2f;
+    readonly float punish = -1f;
+    int caseId = 0;
+    //float startZ = 2f;
+    //float startX = 1f;
+
     //bool idle = true;
 
     //(Vector3 pos, Vector3 rot)[] cases = new (Vector3 pos, Vector3 rot)[] {
@@ -28,71 +35,96 @@ public class SearchLightAgent : Agent
 
     private void Awake()
     {
-        startPos = transform.localPosition;
-        Academy.Instance.OnEnvironmentReset += ResetPlayer;
+        startPosPlayer = transform.localPosition;
+        activeObject = cases[0];
+        startPosObject = activeObject.transform.localPosition;
+        //Academy.Instance.OnEnvironmentReset += ResetPlayer;
     }
 
     public override void OnEpisodeBegin()
     {
         Debug.Log("new episode");
+        transform.localPosition = startPosPlayer;
+        caseId = 0;
+        NextCase();
     }
 
-    public void ResetPlayer()
+    //public void ResetPlayer()
+    //{
+    //    transform.localPosition = startPos;
+    //    //transform.localEulerAngles = Vector3.zero;
+    //    //int caseId = (int)Academy.Instance.EnvironmentParameters.GetWithDefault("case_id", 0);
+    //    //target.transform.localPosition = cases[caseId].pos;
+    //    //target.transform.localEulerAngles = cases[caseId].rot;
+    //    //idle = false;
+    //    target.transform.localPosition = GetRandomPosition();
+    //}
+
+    void NextCase()
     {
-        transform.localPosition = startPos;
-        transform.localEulerAngles = Vector3.zero;
-        //int caseId = (int)Academy.Instance.EnvironmentParameters.GetWithDefault("case_id", 0);
-        //target.transform.localPosition = cases[caseId].pos;
-        //target.transform.localEulerAngles = cases[caseId].rot;
-        //idle = false;
-        target.transform.localPosition = GetRandomPosition();
+        if (caseId >= cases.Length) {
+            EndEpisode();
+            return;
+        }
+        activeObject.SetActive(false);
+        activeObject = cases[caseId];
+        activeObject.transform.localPosition = startPosObject;
+        activeObject.SetActive(true);
+        caseId++;
     }
 
     private void OnTriggerEnter(Collider other)
     {
         //if (idle) return;
-        if (other.gameObject == target)
+        if (other.gameObject.CompareTag("Reward"))
         {
             AddReward(reward);
             Debug.Log("you win");
-            target.transform.localPosition = GetRandomPosition();
+            //target.transform.localPosition = GetRandomPosition();
         }
         else
         {
             AddReward(punish);
             Debug.Log("you lose");
         }
+        NextCase();
         //idle = true;
         //EndEpisode();
     }
 
-    private Vector3 GetRandomPosition()
-    {
-        Vector3 p = new Vector3(Random.Range(-bound, bound), startPos.y, Random.Range(-bound, bound));
-        while (!ValidatePosition(p)) p = new Vector3(Random.Range(-bound, bound), startPos.y, Random.Range(-bound, bound));
-        return p;
-    }
+    //private Vector3 GetRandomPosition()
+    //{
+    //    Vector3 p = new Vector3(Random.Range(-bound, bound), startPos.y, Random.Range(-bound, bound));
+    //    while (!ValidatePosition(p)) p = new Vector3(Random.Range(-bound, bound), startPos.y, Random.Range(-bound, bound));
+    //    return p;
+    //}
 
-    private bool ValidatePosition(Vector3 p)
-    {
-        Vector3 p2 = transform.localPosition;
-        if (Mathf.Abs(p2.x - p.x) < margin && Mathf.Abs(p2.z - p.z) < margin) return false;
+    //private bool ValidatePosition(Vector3 p)
+    //{
+    //    Vector3 p2 = transform.localPosition;
+    //    if (Mathf.Abs(p2.x - p.x) < margin && Mathf.Abs(p2.z - p.z) < margin) return false;
 
-        foreach (GameObject g in obstacles)
-        {
-            p2 = g.transform.localPosition;
-            if (Mathf.Abs(p2.x - p.x) < margin && Mathf.Abs(p2.z - p.z) < margin) return false;
-        }
-        return true;
+    //    foreach (GameObject g in obstacles)
+    //    {
+    //        p2 = g.transform.localPosition;
+    //        if (Mathf.Abs(p2.x - p.x) < margin && Mathf.Abs(p2.z - p.z) < margin) return false;
+    //    }
+    //    return true;
+    //}
+
+    private void FixedUpdate()
+    {
+        activeObject.transform.position -= Time.fixedDeltaTime * speed * activeObject.transform.forward;
+        if (activeObject.transform.localPosition.z < startPosPlayer.z) NextCase();
     }
 
     public override void OnActionReceived(ActionBuffers vectorAction)
     {   
         //if (idle) return;
-        transform.position += Time.fixedDeltaTime * speed * vectorAction.ContinuousActions[0] * transform.forward;
-        transform.Rotate(0, Time.fixedDeltaTime * rotSpeed * (vectorAction.ContinuousActions[2] - vectorAction.ContinuousActions[1]), 0);
+        //transform.position += Time.fixedDeltaTime * speed * vectorAction.ContinuousActions[0] * transform.forward;
+        //transform.Rotate(0, Time.fixedDeltaTime * rotSpeed * (vectorAction.ContinuousActions[2] - vectorAction.ContinuousActions[1]), 0);
         //transform.position += Time.fixedDeltaTime * speed * (vectorAction.ContinuousActions[0] - vectorAction.ContinuousActions[1]) * transform.forward;
-        //transform.position += Time.fixedDeltaTime * speed * (vectorAction.ContinuousActions[2] - vectorAction.ContinuousActions[3]) * transform.right;
+        transform.position += Time.fixedDeltaTime * speed * (vectorAction.ContinuousActions[1] - vectorAction.ContinuousActions[0]) * transform.right;
     }
 
     public override void Heuristic(in ActionBuffers actionsOut)
@@ -101,9 +133,10 @@ public class SearchLightAgent : Agent
         //float v = Input.GetAxis("Vertical");
         float h = Input.GetAxis("Horizontal");
         //if (v > 0) continuousActionsOut[0] = v;
+        //else continuousActionsOut[0] = 0;
         //else continuousActionsOut[1] = -v;
-        if (h > 0) continuousActionsOut[2] = h;
-        else continuousActionsOut[1] = -h;
-        continuousActionsOut[0] = Input.GetKey(KeyCode.Space) ? 1.0f : 0.0f;
+        if (h > 0) continuousActionsOut[1] = h;
+        else continuousActionsOut[0] = -h;
+        //continuousActionsOut[0] = Input.GetKey(KeyCode.Space) ? 1.0f : 0.0f;
     }
 }
